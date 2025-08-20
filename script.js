47a0c634f5ed4217a3aa3cbc3590eaa606130657
clena <storeclena@gmail.com>
2025-08-20 15:54:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+<header class="header">
+<div class="bar">
+<a class="logo" href="index.html" aria-label="SIM Contabilidade">
+<img alt="Logo" src="https://i.imgur.com/3qkKf7S.png" />
+<span>SIM • Contabilidade Inteligente</span>
+</a>
+
+
+<nav class="nav" aria-label="principal">
+<a href="index.html">Home</a>
+<a href="empresa.html">Empresa</a>
+<a href="servicos.html">Serviços</a>
+<a href="noticias.html">Notícias</a>
+<a href="informativo.html">Informativo SM</a>
+<a href="links.html">Links Úteis</a>
+<a href="contato.html">Contato</a>
+</nav>
+
+
+<div class="login-wrap">
+<button class="login-btn" onclick="Login.open()">Área do Cliente</button>
+</div>
+</div>
+</header>
